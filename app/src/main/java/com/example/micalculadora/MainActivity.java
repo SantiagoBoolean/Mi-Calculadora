3961cda973eacaf8160d503917622cfe3ff31ef9
SantiagoBoolean <desousafragososantiago@gmail.com>
2021-03-01 07:46:29 -0300

    Mi Calculadora

---
+++ app/src/main/java/com/example/micalculadora/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.micalculadora;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.etInput)
+    EditText etInput;
+    @BindView(R.id.contentMain)
+    RelativeLayout contentMain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.btnSeven, R.id.btnFour, R.id.btnOne, R.id.btnEight, R.id.btnfive, R.id.btnTwo,
+            R.id.btnNine, R.id.btnSix, R.id.btnThree, R.id.btnPoint, R.id.btnZero})
+    public void onClickeNumbers(View view) {
+        final String valStr = ((Button)view).getText().toString();
+        switch (view.getId()) {
+            case R.id.btnZero:
+            case R.id.btnOne:
+            case R.id.btnTwo:
+            case R.id.btnThree:
+            case R.id.btnFour:
+            case R.id.btnfive:
+            case R.id.btnSix:
+            case R.id.btnSeven:
+            case R.id.btnEight:
+            case R.id.btnNine:
+                etInput.getText().append(valStr);
+                break;
+            case R.id.btnPoint:
+                break;
+        }
+    }
+}
